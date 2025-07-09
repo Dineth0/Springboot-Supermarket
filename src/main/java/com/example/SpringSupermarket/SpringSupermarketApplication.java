386b5f230e26a64e5c012b2f3e2a31df84ee7238
Dineth0 <dinethnakandala10@gmail.com>
2025-07-09 15:42:56 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringSupermarket/SpringSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringSupermarket/SpringSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
