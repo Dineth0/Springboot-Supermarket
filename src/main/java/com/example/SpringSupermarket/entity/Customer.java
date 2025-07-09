31dad693ed5fea1210d408fc22f31549c7911133
Dineth0 <dinethnakandala10@gmail.com>
2025-07-09 15:54:32 +0530

    create customer table

---
+++ src/main/java/com/example/SpringSupermarket/entity/Customer.java
@@ -0,0 +1,23 @@
+package com.example.SpringSupermarket.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int CusId;
+    private String CusName;
+    private String CusAddress;
+    private int CusPhone;
+}
